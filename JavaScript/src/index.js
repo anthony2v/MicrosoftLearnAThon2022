6b70e17a0bb7ddc356cf0ae337db278f41fbe36c
Anthony <anthony_2v@hotmail.com>
2021-12-27 14:39:56 -0500

    Debugging address-parser

--- JavaScript/src/index.js
+++ JavaScript/src/index.js
@@ -1,1 +1,6 @@
+const { parse } = require("./adress-parser");
+
 console.log('Welcome to this application');
+const myOrder = "I want to order: 5 sushi to address: 1234 Denis Montroyal and here is my payment info: giftcard";
+const myParsedOrder = parse(myOrder);
+console.log(myParsedOrder.address);
